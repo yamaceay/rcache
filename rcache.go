7726f51878744c9d1e6db2f3546447f45162401a
yay <yamac.ay@scandio.de>
2022-08-07 09:10:11 +0200

    v1.1.0

--- rcache.go
+++ rcache.go
@@ -1,12 +1,44 @@
 package rcache
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
-func Send(address string, method string, key string, value string) (string, error) {
+// Is equal to Send("set", address, "", "")
+func Keys(address string) ([]string, error) {
+	var keys []string
+	if response, err := Send(address, "get", "", ""); err != nil {
+		return []string{}, fmt.Errorf("unable to fetch all keys: %s", err)
+	} else if err := json.Unmarshal([]byte(response), &keys); err != nil {
+		return []string{}, fmt.Errorf("cannot unmarshal keys: %s", err)
+	}
+	return keys, nil
+}
+
+// Is equal to Send("get", address, key, "")
+func Get(address string, key string) (string, error) {
+	return Send(address, "get", key, "")
+}
+
+// Is equal to Send("set", address, key, value)
+func Set(address string, key string, value string) (string, error) {
+	return Send("set", address, key, value)
+}
+
+// Creates a HTTP request and returns the response string
+//
+// Arguments:
+//  * address string
+//  * method string
+//  * key string
+//  * value string
+// Returns:
+// 	* response string
+// 	* err error
+func Send(method string, address string, key string, value string) (string, error) {
 	fullPath := fmt.Sprintf("%s/%s", address, method)
 
 	req, err := http.NewRequest("GET", fullPath, nil)
